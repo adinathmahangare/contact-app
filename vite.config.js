f64670576314de2acacd72462af294b10a7e81a4
Adinath Mahangare <adinathmahangare914@gmail.com>
2024-03-03 13:05:10 +0530

    Publish

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Set the limit to 1000 kB (1 MB) instead of the default 500 kB
+  },
 })
+
